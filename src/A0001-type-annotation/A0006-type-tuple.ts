c69783bb7420e27b67992bd9c1dc96920b025b7c
victorzix <linkgamesplays@gmail.com>
2023-07-24 13:28:09 -0300

    resting

---
+++ src/A0001-type-annotation/A0006-type-tuple.ts
@@ -0,0 +1,10 @@
+const dadosCliente1: [number, string] = [1, 'Victor']
+const dadosCliente2: [number, string, string?] = [1, 'Victor', 'Raphael']
+
+const maq: Array<string | number> = [1, 'victor', 73]
+
+dadosCliente1[0] = 100;
+dadosCliente1[1] = 'Carlos';
+
+console.log(dadosCliente1)
+console.log(dadosCliente2)
